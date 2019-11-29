8cafa782f9f7d2f30f3a044f5acb02c14c9e9ada
pavol vasko <pavol.vasko@sabstt.com>
2019-11-29 17:28:50 +0000

    prefix

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,8 +1,8 @@
 import { User } from '../user.model';
 import { Action } from '@ngrx/store';
 
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
+export const LOGIN = '[Auth] LOGIN';
+export const LOGOUT = '[Auth] LOGOUT';
 
 
 export class Login implements Action {
